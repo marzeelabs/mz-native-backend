b32cb74bcbabafa9768e3937faf696d6559fe0d8
Luís Miguel <luism@marzeelabs.org>
2019-02-13 13:57:54 +0000

    Create profile endpoint

--- web/modules/mzdrupal_frontend/src/Controller/UserController.php
+++ web/modules/mzdrupal_frontend/src/Controller/UserController.php
@@ -2,27 +2,45 @@
 
 namespace Drupal\mzdrupal_frontend\Controller;
 
-use Drupal\user\Controller\UserController as UserControllerOriginal;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\user\Entity\User;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
-/**
- * Controller routines for user routes.
- */
-class UserController extends UserControllerOriginal {
+class UserController extends ControllerBase {
+
+  private $current_user;
 
   /**
    * {@inheritdoc}
    */
-  public function userPage() {
+  public function __construct(AccountProxyInterface $current_user) {
+    $this->current_user = $current_user;
+  }
 
-    $route_parameters = [
-      'user' => $this->currentUser()->id(),
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    // We assume the user is logged in, as that's a requirement for this route.
+    $current_user = $container->get('current_user');
+    return new static($current_user);
+  }
+
+  /**
+   * Generate.
+   *
+   * @return string
+   */
+  public function info() {
+    $user = User::load($this->current_user->id());
+
+    $response = [
+      'name' => $user->getUsername(),
     ];
 
-    $options = [
-      'query' => \Drupal::request()->query->all(),
-    ];
-
-    return $this->redirect('entity.user.canonical', $route_parameters, $options);
+    return new JsonResponse($response);
   }
 
 }
